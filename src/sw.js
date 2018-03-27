9cc9f138e7393f7624fdfbc3fecc870dd2096543
E J Muis <Emiel@034.143.028.145.hva.nl>
2018-03-27 10:42:59 +0200

    Hijacked SW.js directly

---
+++ src/sw.js
@@ -0,0 +1,6 @@
+
+self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
+
+self.addEventListener('fetch', event => {
+    event.respondWith(new Response('hijacked directly!'));
+});
